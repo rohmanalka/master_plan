d6862f376fb5ef70d6b829585a3e6bb49a53c542
rohmanalka <rohmanalka06@gmail.com>
2025-10-29 14:16:43 +0700

    feat: add dispose method

--- lib/views/plan_screen.dart
+++ lib/views/plan_screen.dart
@@ -22,6 +22,12 @@ class _PlanScreenState extends State<PlanScreen> {
   }
 
   @override
+  void dispose() {
+    scrollController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       // ganti ‘Namaku' dengan Nama panggilan Anda
